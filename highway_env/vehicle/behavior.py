2a62c2eb7b2b0a5f5962135174ae980bd945d263
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-10-03 15:19:15 +0200

    pass params to vehicle class & peds dont stop to vehicles

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -52,10 +52,12 @@ class IDMVehicle(ControlledVehicle):
                  target_speed: float = None,
                  route: Route = None,
                  enable_lane_change: bool = True,
-                 timer: float = None):
+                 timer: float = None,
+                 params = {}):
         super().__init__(road, position, heading, speed, target_lane_index, target_speed, route)
         self.enable_lane_change = enable_lane_change
         self.timer = timer or (np.sum(self.position)*np.pi) % self.LANE_CHANGE_DELAY
+        self.params = params
 
     def randomize_behavior(self):
         pass
@@ -138,7 +140,7 @@ class IDMVehicle(ControlledVehicle):
         acceleration = self.COMFORT_ACC_MAX * (
                 1 - np.power(max(ego_vehicle.speed, 0) / ego_target_speed, self.DELTA))
 
-        if front_vehicle:
+        if front_vehicle and not self.LENGTH < 2.1:
             d = ego_vehicle.lane_distance_to(front_vehicle)
             acceleration -= self.COMFORT_ACC_MAX * \
                 np.power(self.desired_gap(ego_vehicle, front_vehicle) / utils.not_zero(d), 2)
